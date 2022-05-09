cc2d4a5bbbf6c2a60ca8d07029a1244b3464f3e1
vitor-brs <silvarb.vitor@gmail.com>
2022-05-09 15:43:42 -0300

    Update status agenda

---
+++ public/js/editar/btnStatus.js
@@ -0,0 +1,44 @@
+let varId
+function clickStatus(idBtn){
+    
+    return varId = idBtn.id
+    
+}
+function changeStatus(params, liStatusList) {
+    const el = document.getElementById(params)
+    el.innerHTML = liStatusList
+    el.value = liStatusList
+    
+    switch (liStatusList) {
+        case 'Agendado':
+            remove(el)
+            el.classList.add('badge-secondary')
+            break;
+        case 'Confirmado':
+            remove(el)
+            el.classList.add('badge-primary')
+            break;
+        case 'Concluído':
+            remove(el)
+            el.classList.add('badge-success')
+            break;
+        case 'Faltou':
+            remove(el)
+            el.classList.add('badge-warning')
+            break;
+        case 'Cancelado':
+            remove(el)
+            el.classList.add('badge-danger')
+            break;
+        default:
+            break;
+    }
+}
+
+function remove(el) {
+    el.classList.remove('badge-secondary')
+    el.classList.remove('badge-primary')
+    el.classList.remove('badge-success')
+    el.classList.remove('badge-warning')
+    el.classList.remove('badge-danger')
+}
